495e0f0bce2f61f9d01c61cc644803b53b14fa68
Ryan James <realcoder2023@gmail.com>
2025-03-03 13:14:46 -0500

    added first part of Word Problem Math Program

---
+++ src/Names/Names1.ts
@@ -0,0 +1,19 @@
+﻿export interface Name1 {
+
+
+    getRandomName(randName: string[]):void
+}
+
+export class MyNameFirst implements Name1 {
+
+    getRandomName(randName: string[]) {
+
+        const index =
+            Math.floor(Math.random() * randName.length);
+
+
+        return randName[index];
+    }
+
+
+}
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import {MyNameFirst} from "./Names/Names1";
+
+let name1:string[] = []
+
+name1.push("John");
+name1.push("Paul");
+name1.push("Joey");
+name1.push("Justin");
+
+
+
+const name = new MyNameFirst();
+
+name.getRandomName(name1);
